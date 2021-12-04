2bf321cbefb301365ef5c890add598d0f9b57b1d
Do Lee <do.lee.y@gmail.com>
2021-12-03 22:29:44 -0800

    First full update push.

--- app/run.py
+++ app/run.py
@@ -2,17 +2,20 @@ import json
 import plotly
 import pandas as pd
 
+import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from sklearn.base import BaseEstimator, TransformerMixin
 
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
+import joblib
 from sqlalchemy import create_engine
 
 
 app = Flask(__name__)
+
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -25,19 +28,46 @@ def tokenize(text):
 
     return clean_tokens
 
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    @staticmethod
+    def starting_verb(text):
+        # tokenize by sentences
+        sentence_list = nltk.sent_tokenize(text)
+
+        for sentence in sentence_list:
+            # tokenize each sentence into words and tag part of speech
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+
+            # index pos_tags to get the first word and part of speech tag
+            first_word, first_tag = pos_tags[0]
+
+            # return true if the first word is an appropriate verb or RT for retweet
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        # apply starting_verb function to all values in X
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
+
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../UdacityDisasterResponse.db')
+df = pd.read_sql_table('messages', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../nlp_multi_classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,41 +1,123 @@
+import re
 import sys
+import pickle
+import pandas as pd
+from sqlalchemy import create_engine
+import warnings
+warnings.filterwarnings('ignore')
+
+import nltk
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 
 def load_data(database_filepath):
-    pass
+    # load data from database
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table('messages', engine)
+    # remove rows with 2's under related column before split into X and y
+    df = df[df.related != 2]
+    # split data
+    X = df['message']
+    y = df.iloc[:, 4:]
+    category_names = y.columns.values
+    return X, y, category_names
 
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+
+    # identify urls
+    detected_urls = re.findall(url_regex, text)
+    # replace urls
+    for detected_url in detected_urls:
+        text = text.replace(detected_url, "urlplaceholder")
+
+    # extract word tokens from provided text
+    tokens = nltk.word_tokenize(text)
+    # lemmanitize by removing inflectional and derivationally related forms of a word
+    lemmatizer = nltk.WordNetLemmatizer()
+    # list of cleaned tokens
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
+
+    return clean_tokens
+
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+    # extract the starting verb of a sentence (new feature)
+    @staticmethod
+    def starting_verb(text):
+        # tokenize by sentences
+        sentence_list = nltk.sent_tokenize(text)
+
+        for sentence in sentence_list:
+            # tokenize each sentence into words and tag part of speech
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            # index pos_tags to get the first word and part of speech tag
+            first_word, first_tag = pos_tags[0]
+            # return true if the first word is an appropriate verb or RT for retweet
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        # apply starting_verb function to all values in X
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('count_vectorizer', CountVectorizer(tokenizer = tokenize)),
+                ('tfidf_transformer', TfidfTransformer())
+            ])),
+
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return pipeline
 
 
-def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+def evaluate_model(model, X_test, y_test, category_names):
+    y_pred_test = model.predict(X_test)
+    print(classification_report(y_test.values, y_pred_test, target_names=category_names))
 
 
 def save_model(model, model_filepath):
-    pass
+    with open(model_filepath, 'wb') as pickle_file:
+        pickle.dump(model, pickle_file)
 
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
-        X, Y, category_names = load_data(database_filepath)
-        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
+        X, y, category_names = load_data(database_filepath)
+
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
-        model.fit(X_train, Y_train)
-        
+        model.fit(X_train, y_train)
+
         print('Evaluating model...')
-        evaluate_model(model, X_test, Y_test, category_names)
+        evaluate_model(model, X_test, y_test, category_names)
 
         print('Saving model...\n    MODEL: {}'.format(model_filepath))
         save_model(model, model_filepath)
@@ -43,9 +125,9 @@ def main():
         print('Trained model saved!')
 
     else:
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
+        print('Please provide the filepath of the disaster messages database '
+              'as the first argument and the filepath of the pickle file to '
+              'save the model to as the second argument. \n\nExample: python '
               'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
 
 
